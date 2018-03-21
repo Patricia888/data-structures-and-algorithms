80be47a6dd95e3b4a5eefdf3d6d18f889cc552b4
Patricia G Raftery <praftery8@gmail.com>
2018-03-20 21:29:02 -0700

    started shift array

---
+++ challanges/shift_array/shift-array.py
@@ -0,0 +1,16 @@
+
+
+newArray = []
+
+def insertShiftArray(givenArray, addedValue):
+    # find length
+
+    # using if/elif statements, put numbers less than middle indice in newArray
+
+    # put addedValue in middle index
+
+    # put all other numbers at previous indice + 1
+
+    
+    
+    
