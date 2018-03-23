baa8bee178d71a80c2ab197308c13c5c3ccebde4
Patricia G Raftery <praftery8@gmail.com>
2018-03-22 20:51:31 -0700

    largest product

---
+++ challanges/largest_product/largest_product.py
@@ -0,0 +1,6 @@
+def LargestProduct(array):
+    # for iterates over outer array
+    # for loop iterates over inner array
+        # multiply the 2 numbers
+        # compare to see which product is biggest
+    return running_largest_product
