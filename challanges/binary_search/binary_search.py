ce20fed911db767ddae25a0dde61fb639aa6cb58
Patricia G Raftery <praftery8@gmail.com>
2018-03-25 16:35:19 -0700

    binary

--- challanges/binary_search/binary_search.py
+++ challanges/binary_search/binary_search.py
@@ -1,19 +1,6 @@
-
-
-
 def BinarySearch(sorted_array, search_key):
-    # find middle index
-        # middle_index = sorted_array.length / 2
-
-    # if search key == sorted_array[middle_index]
-        # done! return middle_index
-
-    # elif search key > sorted_array[middle_index]
-        # search_key must be in top half of list
-        # keep searching and splitting until match is found
-
-    # else search key must be in bottom half of list
-        # keep searching and splitting until match is found
-
-    # return -1 if no match found
-
+    answer = -1
+    for i in range(len(sorted_array)):
+        if sorted_array[i] == search_key:
+            answer = i
+    return answer
