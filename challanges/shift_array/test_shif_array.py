bde16e06d4d761fca77173f5f295366dfb4d8106
Patricia G Raftery <praftery8@gmail.com>
2018-03-25 19:07:34 -0700

    changed file name

--- challanges/shift_array/test_shif_array.py
+++ challanges/shift_array/test_shif_array.py
@@ -1,14 +1,14 @@
-import shift-array
+import shift_array
 import pytest
 
 
 def test_insertShiftArray_odd():
-    assert shift-array.insertShiftArray([1,2,3,4,5,6,7,8,9], 'X') == [1, 2, 3, 4, 'X', 5, 6, 7, 8, 9]
+    assert shift_array.insertShiftArray([1,2,3,4,5,6,7,8,9], 'X') == [1, 2, 3, 4, 'X', 5, 6, 7, 8, 9]
 
 
 def test_insertShiftArray_with_even():
-    assert shift-array.insertShiftArray([1,2,3,4,5,6,7,8], 'X') == [1, 2, 3, 4, 'X', 5, 6, 7, 8]
+    assert shift_array.insertShiftArray([1,2,3,4,5,6,7,8], 'X') == [1, 2, 3, 4, 'X', 5, 6, 7, 8]
 
 
 def test_insertShiftArray_with_empty():
-    assert shift-array.insertShiftArray([], 'X') == ['X']
+    assert shift_array.insertShiftArray([], 'X') == ['X']
