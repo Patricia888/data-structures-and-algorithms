b8646852a15c440c61de69711b579ae426a06d6f
Patricia G Raftery <praftery8@gmail.com>
2018-03-29 09:27:10 -0700

    linked list

--- data_structures/linked_list/linked_list.py
+++ data_structures/linked_list/linked_list.py
@@ -1,24 +1,25 @@
-from node import node
+from node import Node
 
 
-class linked_list:
+class LinkedList:
     """
     Singly linked list
     """
     def __init__(self, iter=[]):
-        self._current = None
+        # self._current = None
         self.head = None
-        self._size = 0 # or length
-
-    # l = LinkedList([1, 2, 3, 4])
-    for item in reversed(iter):
-        self.insert(item)
-    # l.head => 1 => 2 => 3 => 4
-    # puts in front, instead of end
+        self._size = 0  # or length
+        if type(iterable) is not list:
+            raise TypeError('Invalid iterable')
+        for item in iterable:
+                self.insert(item)
 
     def __repr__(self):
         # assuming head will have a val
-        return '<head> => {}'.format(self.head.val)
+        return f'<head> is {self.head.val}'
+
+    def __str__(self):
+        return self.__repr__
 
     def __len__(self):
         return self._size
@@ -27,3 +28,13 @@ class linked_list:
         node = Node(val)
         node.next = self.head
         self.head = node
+        self._size += 1
+
+    def find(self, val):
+        current = self.head
+        while current:
+            if current.val == val:
+                return True
+            current = current._next
+
+        return False
