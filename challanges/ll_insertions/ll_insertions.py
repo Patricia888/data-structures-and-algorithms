d12977689a7105df73c02608b155ce1a72b2733f
Patricia G Raftery <praftery8@gmail.com>
2018-03-30 09:47:53 -0700

    insert before and after written

--- challanges/ll_insertions/ll_insertions.py
+++ challanges/ll_insertions/ll_insertions.py
@@ -26,6 +26,7 @@ class LinkedList:
         return self._size
 
     def insert(self, val):
+        # self.head = Node(val, self.head)
         node = Node(val)
         node._next = self.head
         self.head = node
@@ -39,3 +40,42 @@ class LinkedList:
             current = current._next
 
         return False
+
+    def append(self, val):
+        if self.head is None:
+            self.insert(val)
+        else:
+            current = self.head
+            while current is not None:
+                if current._next is None:
+                    current._next = Node(val)
+                    self._size += 1
+                    break
+                current = current._next
+
+    def insert_before(self, val, newVal):
+        current = self.head
+        previous = None
+        while current:
+            if current.val == val:
+                if previous is None:
+                    self.insert(newVal)
+                else:
+                    new_node = Node(newVal)
+                    new_node._next = current
+                    previous._next = new_node
+                    self._size += 1
+                break
+            previous = current
+            current = current._next
+
+    def insert_after(self, val, newVal):
+        current = self.head
+        while current:
+            if current.val == val:
+                position = current._next
+                current._next = Node(newVal)
+                current._next._next = position
+                self._size += 1
+                break
+            current = current._next
