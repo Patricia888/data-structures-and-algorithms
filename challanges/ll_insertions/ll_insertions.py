18170b3634a6afd4312eec51db1fb88506a53ad3
Patricia G Raftery <praftery8@gmail.com>
2018-03-30 09:32:36 -0700

    tests, ll updated

--- challanges/ll_insertions/ll_insertions.py
+++ challanges/ll_insertions/ll_insertions.py
@@ -1,4 +1,4 @@
-from node import node
+from .node import Node
 
 
 class LinkedList:
@@ -6,29 +6,36 @@ class LinkedList:
     Single linked list
     """
     def __init__(self, iter=[]):
-        # below will use later
         # self._current = None
         self.head = None
-        self._size = 0 # or length
+        self._size = 0
+        if type(iter) is not list:
+            raise TypeError('Invalid iterable')
 
-    # l = LinkedList([1, 2, 3, 4])
-    for item in reversed(iter):
-        self.insert(item)
-    # l.head => 1 => 2 => 3 => 4
-    # puts in front, instead of end
+        for item in reversed(iter):
+            self.insert(item)
 
     def __repr__(self):
         # assuming head will have a val
         return '<head> => {}'.format(self.head.val)
 
+    def __str__(self):
+        return self.__repr__
+
     def __len__(self):
         return self._size
 
-    # 3 ways to Insert (at start)
     def insert(self, val):
-        # node = Node(val, self.head)
-        # self.head = node
-
         node = Node(val)
-        node.next = self.head
+        node._next = self.head
         self.head = node
+        self._size += 1
+
+    def find(self, val):
+        current = self.head
+        while current is not None:
+            if current.val == val:
+                return True
+            current = current._next
+
+        return False
