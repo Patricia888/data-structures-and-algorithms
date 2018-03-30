ace967b5d34441f1d9c68bd54d085b99156e59d1
Patricia G Raftery <praftery8@gmail.com>
2018-03-30 09:11:49 -0700

    add basic ll

---
+++ challanges/ll_find_loop/conftest.py
@@ -0,0 +1,13 @@
+import pytest
+from .ll_find_loop import LinkedList as LL
+# from snakes_cafe import snakescafe as sc
+
+
+@pytest.fixture
+def empty_ll():
+    return LL()
+
+
+@pytest.fixture
+def small_ll():
+    return LL([1, 2, 3, 4])
---
+++ challanges/ll_find_loop/ll_find_loop.py
@@ -0,0 +1,41 @@
+from .node import Node
+
+
+class LinkedList:
+    """
+    Singly linked list
+    """
+    def __init__(self, iter=[]):
+        # self._current = None
+        self.head = None
+        self._size = 0  # or length
+        if type(iter) is not list:
+            raise TypeError('Invalid iterable')
+        
+        for item in reversed(iter):
+            self.insert(item)
+
+    def __repr__(self):
+        # assuming head will have a val
+        return '<head> => {}'.format(self.head.val)
+
+    def __str__(self):
+        return self.__repr__
+
+    def __len__(self):
+        return self._size
+
+    def insert(self, val):
+        node = Node(val)
+        node._next = self.head
+        self.head = node
+        self._size += 1
+
+    def find(self, val):
+        current = self.head
+        while current is not None:
+            if current.val == val:
+                return True
+            current = current._next
+
+        return False
