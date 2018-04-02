623aa48598bca8d846e01e15ed520d2bd7fca16a
Patricia G Raftery <praftery8@gmail.com>
2018-04-02 09:30:54 -0700

    whiteboard interview

--- data_structures/queue/queue.py
+++ data_structures/queue/queue.py
@@ -1,6 +1,7 @@
 from .node import Node
 
 
+# a queue is only aware of 'the back'
 class Queue:
     """create a queue"""
     def __init__(self, iterable=[]):
@@ -14,9 +15,22 @@ class Queue:
 
     def __repr__(self):
         return f'Front of queue is {self.front.val}'
+        # print front of queue
+        # current = self.back
+        # while current._next:
+        #     current = current._next
 
     def __str__(self):
+        # print back
+        # return 'Queue back: {}'.format(self.back.val)
+        # print front
         return f'Front of queue is {self.front.val}'
 
     def __len__(self):
         return self._size
+
+    def enqueue(self, val):
+        self.back = Node(val, self.back)
+
+    def dequeue(self):
+        pass
