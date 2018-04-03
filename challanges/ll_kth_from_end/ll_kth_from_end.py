b09dae1e4d1d14c3e8e5ff0bd7d1b99fcfcfccf4
Patricia G Raftery <praftery8@gmail.com>
2018-04-03 09:36:05 -0700

    added find method

--- challanges/ll_kth_from_end/ll_kth_from_end.py
+++ challanges/ll_kth_from_end/ll_kth_from_end.py
@@ -1,4 +1,4 @@
-from node import Node
+from .node import Node
 
 
 class LinkedList:
@@ -6,20 +6,34 @@ class LinkedList:
     create a linked list
     """
     def __init__(self, iterable=[]):
-        """Constructor for the LinkedList object"""
         self.head = None
         self._size = 0
         if type(iterable) is not list:
             raise TypeError('Invalid iterable')
-        for item in iterable:
+
+        for item in reversed(iter):
                 self.insert(item)
 
     def __repr__(self):
-        return f'<head> is {self.head.val}'
+        return '<head> => {}'.format(self.head.val)
 
     def __str__(self):
         return self.__repr__
 
     def __len__(self):
-        """Return the size of the linked list"""
         return self._size
+
+    def insert(self, val):
+        node = Node(val)
+        node._next = self.head
+        self.head = node
+        self._size += 1
+
+    def find(self, val):
+        current = self.head
+        while current is not None:
+            if current.val == val:
+                return True
+            current = current._next
+
+        return False
