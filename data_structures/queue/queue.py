8b9e406a2e1dfd9be17aedc12bd03ef97ccc9453
Patricia G Raftery <praftery8@gmail.com>
2018-04-02 20:36:58 -0700

    file scaffold

--- data_structures/queue/queue.py
+++ data_structures/queue/queue.py
@@ -30,11 +30,53 @@ class Queue:
         return self._size
 
     def enqueue(self, val):
-        self.back = Node(val, self.back)
+        node = Node(val)
+
+        if self._size == 0:
+            self.front = self.back = node
+            self._size += 1
+            return node
+
+        self.back.next = self.back = node
+        self._size += 1
+        return node
+
+
+        # # put in queue
+
+        # # takes care of an empty list or list of 1
+        # # don't need else
+        # # if true will automatically break out of the if statement
+        # if self.front is None:
+        #     self.front = self.back = Node(val)
+        #     self._size += 1
+        #     return self.back
+
+        # # takes care of a list that has more than 1 value
+        # self.back = Node(val, self.back)
+        # self._size += 1
+        # return self.back
 
     def dequeue(self):
-        current = self.back
-        while current._next:
-            current = current._next
+        if self._size == 0:
+            raise IndexError('List is empty')
 
-        current.next = None
+        temp = self.front
+        self.front = temp.next
+        self._size -= 1
+        return temp
+
+
+
+        # # remove from queue
+
+        # current = self.back
+        # while current._next:
+        #     prev = current
+        #     current = current._next
+
+        # prev._next = None
+        # return current
+
+        # # do it with front:
+
