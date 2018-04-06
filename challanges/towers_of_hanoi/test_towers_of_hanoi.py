d7fbd43dac4fb540ab69592e0f67822ef8fe334d
Patricia G Raftery <praftery8@gmail.com>
2018-04-05 21:54:05 -0700

    towers and tests started

---
+++ challanges/towers_of_hanoi/test_towers_of_hanoi.py
@@ -0,0 +1,5 @@
+from .towers_of_hanoi import towers_of_hanoi
+
+
+def test_towers_of_hanoi_with_2_disks():
+    pass
---
+++ challanges/towers_of_hanoi/towers_of_hanoi.py
@@ -0,0 +1,7 @@
+def towers_of_hanoi(n):
+    a = []
+    b = []
+    c = []
+    for i in reversed(range(1, n + 1)):
+        # do recursive stuff
+        
