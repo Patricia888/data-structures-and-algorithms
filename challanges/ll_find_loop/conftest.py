738dd190f9b41ba4cf0d9d70cba1e3853cac0cd5
Patricia G Raftery <praftery8@gmail.com>
2018-04-08 14:55:27 -0700

    started has loop method, confest up to date with previous branches

--- challanges/ll_find_loop/conftest.py
+++ challanges/ll_find_loop/conftest.py
@@ -1,6 +1,5 @@
 import pytest
 from .ll_find_loop import LinkedList as LL
-# from snakes_cafe import snakescafe as sc
 
 
 @pytest.fixture
@@ -11,3 +10,13 @@ def empty_ll():
 @pytest.fixture
 def small_ll():
     return LL([1, 2, 3, 4])
+
+
+@pytest.fixture
+def short_ll():
+    return LL([5, 6, 7, 8])
+
+
+@pytest.fixture
+def long_ll():
+    return LL([11, 12, 13, 14, 15, 16])
