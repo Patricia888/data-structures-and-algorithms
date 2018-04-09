c05f8e8164172a7449a79e0c73764b2f9db9b2cb
Patricia G Raftery <praftery8@gmail.com>
2018-04-08 19:04:59 -0700

    passes all tests

--- data_structures/queue/queue.py
+++ data_structures/queue/queue.py
@@ -20,3 +20,24 @@ class Queue:
 
     def __len__(self):
         return self._size
+
+    def enqueue(self, val):
+        try:
+            node = Node(val)
+        except TypeError:
+            return self.back
+        self._size += 1
+        if self.front is None:
+            self.front = node
+            self.back = node
+        self.back._next = node
+        self.back = node
+        return self.front
+
+    def dequeue(self):
+        if self._size == 0:
+            raise IndexError('List is empty')
+        removed_node = self.front
+        self.front = self.front._next
+        self._size -= 1
+        return removed_node.val
