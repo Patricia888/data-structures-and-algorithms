dded78e54b9473daf0136328e5a768d1b1ddf586
Patricia G Raftery <praftery8@gmail.com>
2018-04-08 18:56:56 -0700

    node and conftest done

--- data_structures/queue/conftest.py
+++ data_structures/queue/conftest.py
@@ -6,3 +6,24 @@ from .queue import Queue
 def empty_queue():
     """empty queue"""
     return Queue()
+
+
+@pytest.fixture
+def short_queue():
+    """create a short queue"""
+    q = Queue()
+    q.enqueue(1)
+    q.enqueue(2)
+    q.enqueue(3)
+    q.enqueue(4)
+    q.enqueue(5)
+    return q
+
+
+@pytest.fixture
+def long_queue():
+    """Create a long queue"""
+    q = Queue()
+    for num in range(1, 11):
+        q.enqueue(num)
+    return q
