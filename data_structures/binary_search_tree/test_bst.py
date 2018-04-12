48873fceb89cdc03f0a40363b94d71a30b260b3f
Patricia G Raftery <praftery8@gmail.com>
2018-04-12 09:35:25 -0700

    tree working, 2 tests written

--- data_structures/binary_search_tree/test_bst.py
+++ data_structures/binary_search_tree/test_bst.py
@@ -1,2 +1,18 @@
 from .bst import BST
+from .bst import Node
 import pytest
+
+
+def test_create_node():
+    node = Node(5)
+    assert node.left is None
+    assert node.right is None
+
+
+def test_bst_created_with_iterable():
+    binarytree = BST([1, 2, 3])
+    assert binarytree.root.val == 1
+    assert binarytree.root.right.val == 2
+    assert binarytree.root.right.right.val == 3
+
+
