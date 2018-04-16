9475a19d27f69dd33f121b1d45484da73283c86b
Patricia G Raftery <praftery8@gmail.com>
2018-04-15 21:15:22 -0700

    all tests pass

--- challanges/find_maximum_value_binary_tree/node.py
+++ challanges/find_maximum_value_binary_tree/node.py
@@ -1,13 +1,12 @@
 class Node:
-    """create a node"""
-    def __init__(self, val, next=None):
+    """create a Node"""
+    def __init__(self, val):
         self.val = val
-        self._next = next
-        if val is None:
-            raise TypeError('Must pass a value')
+        self.left = None
+        self.right = None
 
     def __repr__(self):
-        return '{val}'.format(val=self.val)
+        return 'Node Val: {}'.format(self.val)
 
     def __str__(self):
-        return '{val}'.format(val=self.val)
+        return str(self.val)
--- challanges/find_maximum_value_binary_tree/queue.py
+++ challanges/find_maximum_value_binary_tree/queue.py
@@ -1,4 +1,17 @@
-from .node import Node
+class Node:
+    """create a node"""
+    def __init__(self, val, next=None):
+        """Constructor for the Node object"""
+        self.val = val
+        self._next = next
+        if val is None:
+            raise TypeError('Must pass a value')
+
+    def __repr__(self):
+        return '{val}'.format(val=self.val)
+
+    def __str__(self):
+        return '{val}'.format(val=self.val)
 
 
 class Queue:
