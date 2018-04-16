b4debc7388b5c2e3184026755b7b3e7d2a7f1e61
Patricia G Raftery <praftery8@gmail.com>
2018-04-15 20:47:31 -0700

    all tests pass

--- challanges/towers_of_hanoi/towers_of_hanoi.py
+++ challanges/towers_of_hanoi/towers_of_hanoi.py
@@ -1,7 +1,22 @@
 def towers_of_hanoi(n):
+    '''towers of hanoi. moves disks form pole a to the target pole and returns the steps needed to get there.'''
     a = []
     b = []
     c = []
     for i in reversed(range(1, n + 1)):
         # do recursive stuff
-        
+        a.append(i)
+    count = recur(n, a, c, b)
+    return (count, c, b, a)
+
+
+def recur(n, a, c, b):
+    """recursive function that keeps getting called until all disks are at the target destination"""
+    count = 0
+    if n >= 1:
+        first = recur(n - 1, a, b, c)
+        c.append(a.pop())
+        count += 1
+        second = recur(n - 1, b, c, a)
+        count += first + second
+    return count
