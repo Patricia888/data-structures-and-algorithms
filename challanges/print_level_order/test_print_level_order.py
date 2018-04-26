27a9385959bdb8d6f4a58daedb38154a36474ba7
Patricia G Raftery <praftery8@gmail.com>
2018-04-26 09:30:14 -0700

    k ary tree done, test trees created

---
+++ challanges/print_level_order/test_print_level_order.py
@@ -0,0 +1,31 @@
+from k_tree import KTree as KT
+from print_level_order import print_level_order
+import pytest
+
+
+@pytest.fixture
+def single_tree():
+    tree = KT()
+    tree.insert(1, None)
+    return tree
+
+
+@pytest.fixture
+def small_tree():
+    tree = KT()
+    tree.insert(1, None)
+    tree.insert(2, 1)
+    tree.insert(3, 1)
+    return tree
+
+
+@pytest.fixture
+def tree():
+    tree = KT()
+    tree.insert(1, None)
+    tree.insert(2, 1)
+    tree.insert(3, 1)
+    tree.insert(4, 1)
+    tree.insert(5, 1)
+    return tree
+
