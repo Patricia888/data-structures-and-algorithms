9e0bfb9466f9b23ebca5b4de395996ed96f7d4b2
Patricia G Raftery <praftery8@gmail.com>
2018-04-26 09:40:50 -0700

    printing works, all tests pass

--- challanges/print_level_order/print_level_order.py
+++ challanges/print_level_order/print_level_order.py
@@ -1,5 +1,22 @@
-from queue import Queue
+from .queue import Queue
 
 
 def print_level_order(tree):
-    # print nodes in order, with spaces between levels
+    '''print values of all nodes in level order of K-ary tree'''
+    queue = Queue()
+    next_queue = Queue()
+    list_of_all_values = ''
+
+    queue.enqueue(tree.root)
+
+    while queue or next_queue:
+        if not queue:
+            queue, next_queue = next_queue, queue
+            list_of_all_values += '\n'
+
+        current = queue.dequeue()
+        list_of_all_values += f'{current.val} '
+        for child in current.children:
+            next_queue.enqueue(child)
+
+    return list_of_all_values
--- challanges/print_level_order/test_print_level_order.py
+++ challanges/print_level_order/test_print_level_order.py
@@ -1,5 +1,5 @@
-from k_tree import KTree as KT
-from print_level_order import print_level_order
+from .k_tree import KTree as KT
+from .print_level_order import print_level_order
 import pytest
 
 
@@ -29,3 +29,17 @@ def tree():
     tree.insert(5, 1)
     return tree
 
+
+def test_print_level_order(tree):
+    """test print_level_order"""
+    assert print_level_order(tree) == '1 \n2 3 4 5 '
+
+
+def test_print_level_order_small(small_tree):
+    """test print_level_order with small tree"""
+    assert print_level_order(small_tree) == '1 \n2 3 '
+
+
+def test_print_level_order_large(single_tree):
+    """test print_level_order with large tree"""
+    assert print_level_order(single_tree) == '1 '
