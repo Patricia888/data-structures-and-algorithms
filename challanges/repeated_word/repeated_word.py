e6be779625208636bc4ea7b74e62b33b6e86b5fc
Patricia G Raftery <praftery8@gmail.com>
2018-04-30 20:19:42 -0700

    plan

---
+++ challanges/repeated_word/repeated_word.py
@@ -0,0 +1,9 @@
+# use hash table with linked lis
+
+# split string at spaces, filter out dashes, commas, periods (regex?)
+
+# put words in the hash table
+
+# on collision, check if the vals are the same
+
+# if so, that's your answer.
