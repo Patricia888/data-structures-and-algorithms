07323abb8a89542c0fafb3458f65e40043270fc1
Patricia G Raftery <praftery8@gmail.com>
2018-05-14 18:59:11 -0700

    repeated word

--- challanges/repeated_word/repeated_word.py
+++ challanges/repeated_word/repeated_word.py
@@ -1,9 +1,14 @@
-# use hash table with linked lis
+from .hash_table import HashTable
 
-# split string at spaces, filter out dashes, commas, periods (regex?)
 
-# put words in the hash table
-
-# on collision, check if the vals are the same
-
-# if so, that's your answer.
+def repeated_word(lengthy_string):
+    """Return the first repeated word of an lengthy input string."""
+    lengthy_string = lengthy_string.lower()
+    lengthy_list = lengthy_string.split(' ')
+    ht = HashTable(len(lengthy_list))
+    for word in lengthy_list:
+        if ht.get(word):
+            return word
+        else:
+            ht.set(word, word)
+    return False
