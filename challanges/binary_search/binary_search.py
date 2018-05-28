29be026982b9eca0c5681b53ad885e02239dbd0f
Patricia G Raftery <praftery8@gmail.com>
2018-05-28 12:41:51 -0700

    fixing binary search

--- challanges/binary_search/binary_search.py
+++ challanges/binary_search/binary_search.py
@@ -1,6 +1,52 @@
-def BinarySearch(sorted_array, search_key):
-    answer = -1
-    for i in range(len(sorted_array)):
-        if sorted_array[i] == search_key:
-            answer = i
-    return answer
+# Not actual binary search
+
+# def BinarySearch(sorted_array, search_key):
+#     answer = -1
+#     for i in range(len(sorted_array)):
+#         if sorted_array[i] == search_key:
+#             answer = i
+#     return answer
+
+
+# Stack Overflow answer:
+
+def binary_search(array, key):
+    if len(array) == 0:
+        return -1
+
+    bound_high = len(array) - 1
+    bound_low = 0
+    index = 0
+    while bound_low <= bound_high:
+        index = bound_low + (bound_high - bound_low) // 2
+        if array[index] == key:
+            return index
+        elif array[index] < key:
+            bound_low = index + 1
+        else:
+            bound_high = index - 1
+    return -1
+
+
+# Another version:
+
+# def binarySearch(alist, item):
+#     first = 0
+#     last = len(alist)-1
+#     found = False
+
+#     while first <= last and not found:
+#         midpoint = (first + last)//2
+#         if alist[midpoint] == item:
+#             found = True
+#         else:
+#             if item < alist[midpoint]:
+#                 last = midpoint-1
+#             else:
+#                 first = midpoint+1
+
+#     return found
+
+#     testlist = [0, 1, 2, 8, 13, 17, 19, 32, 42,]
+#     print(binarySearch(testlist, 3))
+#     print(binarySearch(testlist, 13))
--- challanges/binary_search/test_binary_search.py
+++ challanges/binary_search/test_binary_search.py
@@ -1,10 +1,23 @@
-from .binary_search import BinarySearch
+# from .binary_search import BinarySearch
+from .binary_search import binary_search
 import pytest
 
 
+# def test_BinarySearch_example_one():
+#     assert BinarySearch([4, 8, 15, 16, 23, 42], 15) == 2
+
+
+# def test_BinarySearch_example_two():
+#     assert BinarySearch([11, 22, 33, 44, 55, 66, 77], 90) == -1
+
+
 def test_BinarySearch_example_one():
-    assert BinarySearch([4, 8, 15, 16, 23, 42], 15) == 2
+    assert binary_search([4, 8, 15, 16, 23, 42], 15) == 2
 
 
-def test_BinarySearch_example_two():
-    assert BinarySearch([11, 22, 33, 44, 55, 66, 77], 90) == -1
+def test_BinarySearch_no_result():
+    assert binary_search([1, 2, 3, 4], 6) == -1
+
+
+def test_BinarySearch_empty_array():
+    assert binary_search([], 5) == -1
